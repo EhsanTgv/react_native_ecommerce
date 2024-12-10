6b6509a27747ab56d225a5857734e118626fdf9b
EhsanTgv <EhsanTgv@gmail.com>
2024-12-10 08:49:41 +0300

    Add flex and nested view

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import {Text, View} from 'react-native';
 
 export default function App() {
     return (
-        <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Hello React Native</Text>
+        <View style={{flex: 0.5, alignItems: 'center', justifyContent: 'center', backgroundColor: '#BBB'}}>
+            <Text style={{fontSize: 20}}>Hello React Native</Text>
+            <Text style={{fontSize: 22}}>Let's program in</Text>
+            <Text style={{fontSize: 25}}>React Native</Text>
         </View>
     )
 }
