65b145a7612271d2f001f93d2ffc7e1504438cf5
EhsanTgv <EhsanTgv@gmail.com>
2024-12-23 10:03:26 +0300

    Practice more with flex and nested views

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
-import {Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={{flex: 0.5, alignItems: 'center', justifyContent: 'center', backgroundColor: '#BBB'}}>
-            <Text style={{fontSize: 20}}>Hello React Native</Text>
-            <Text style={{fontSize: 22}}>Let's program in</Text>
-            <Text style={{fontSize: 25}}>React Native</Text>
+        <View style={{
+            flexDirection: 'column',
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: '#BBB',
+        }}>
+            <View style={{ flexDirection: "row" }}>
+                <Text style={{ fontSize: 20 }}>Hello React Native</Text>
+                <Text style={{ fontSize: 22 }}>Let's program in</Text>
+                <Text style={{ fontSize: 25 }}>React Native</Text>
+            </View>
+
+            <View>
+                <Text style={{ fontSize: 20 }}>Hello React Native</Text>
+                <Text style={{ fontSize: 22 }}>Let's program in</Text>
+                <Text style={{ fontSize: 25 }}>React Native</Text>
+            </View>
         </View>
     )
 }
